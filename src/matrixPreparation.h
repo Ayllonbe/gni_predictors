76c29cf3af17ed9ced101de0f048b9883e8f94d5
Ayllonbe <ayllonbenitez.aaron@gmail.com>
2020-07-10 18:14:40 +0200

    modifiying TO similarity

--- src/matrixPreparation.h
+++ src/matrixPreparation.h
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <omp.h>
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/minmax.hpp>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <armadillo>
@@ -29,56 +30,56 @@ const NetworKit::edgeweight ew_default = 1.0;
 // index - the node index in the created graph
 // private ic - the information content
 class GOTerm{
-    public:
-        std::string id;
-        std::string name;
-        std::string top;
-        NetworKit::node index; // this index corresponds to graph node index.
-        GOTerm(){
-        }
-        GOTerm(std::string& id, std::string& name, std::string& top,NetworKit::node& index){
-            this->id=id;
-            this->name=name;
-            this->top=top;
-            this->index=index;
-        }
-        GOTerm(std::vector<std::string>& termInfo,NetworKit::node& index){
-            this->id=termInfo.at(0);
-            this->name=termInfo.at(1);
-            this->top=termInfo.at(2);
-            this->index=index;
-        }
-        void set_IC(double& val){
-            this->ic=val;
-        }
-        double get_IC(){
-            return this->ic;
-        }
-        void set_child(NetworKit::node c){
-            this->childs.push_back(c);
-        }
-        NetworKit::node get_child(int pos){
-           return this->childs.at(pos);
-        }
-        std::vector<NetworKit::node>& get_childrens(){
-           return this->childs;
-        }
-        void set_parent(NetworKit::node c){
-            this->parents.push_back(c);
-        }
-        NetworKit::node& get_parent(int pos){
-           return this->parents.at(pos);
-        }
-        std::vector<NetworKit::node>& get_parents(){
-           return this->parents;
-        }
+public:
+    std::string id;
+    std::string name;
+    std::string top;
+    NetworKit::node index; // this index corresponds to graph node index.
+    GOTerm(){
+    }
+    GOTerm(std::string& id, std::string& name, std::string& top,NetworKit::node& index){
+        this->id=id;
+        this->name=name;
+        this->top=top;
+        this->index=index;
+    }
+    GOTerm(std::vector<std::string>& termInfo,NetworKit::node& index){
+        this->id=termInfo.at(0);
+        this->name=termInfo.at(1);
+        this->top=termInfo.at(2);
+        this->index=index;
+    }
+    void set_IC(double& val){
+        this->ic=val;
+    }
+    double get_IC(){
+        return this->ic;
+    }
+    void set_child(NetworKit::node c){
+        this->childs.push_back(c);
+    }
+    NetworKit::node get_child(int pos){
+        return this->childs.at(pos);
+    }
+    std::vector<NetworKit::node>& get_childrens(){
+        return this->childs;
+    }
+    void set_parent(NetworKit::node c){
+        this->parents.push_back(c);
+    }
+    NetworKit::node& get_parent(int pos){
+        return this->parents.at(pos);
+    }
+    std::vector<NetworKit::node>& get_parents(){
+        return this->parents;
+    }
 
-    private:
-        double ic;
-       std::vector<NetworKit::node> childs;
-      std::vector<NetworKit::node> parents;
-  //     vector<NetworKit::node> descendants;
- //      vector<NetworKit::node> ancestors;
+private:
+    double ic;
+    std::vector<NetworKit::node> childs;
+    std::vector<NetworKit::node> parents;
+    //     vector<NetworKit::node> descendants;
+    //      vector<NetworKit::node> ancestors;
 
 };
 
@@ -95,13 +96,13 @@ std::vector<GOTerm> getancestors(NetworKit::node& n, robin_hood::unordered_map<N
 
 
 class Root{
-    public:
-        std::string id;
-        long ndescendants;
-        Root(std::string& id, long& ndesc){
-            this->id=id;
-            this->ndescendants=ndesc;
-        }
+public:
+    std::string id;
+    long ndescendants;
+    Root(std::string& id, long& ndesc){
+        this->id=id;
+        this->ndescendants=ndesc;
+    }
 
 };
 
@@ -115,7 +116,7 @@ class Root{
 void reader(std::string go_file, robin_hood::unordered_map<NetworKit::node,GOTerm>& idx2goterm,NetworKit::Graph& go, robin_hood::unordered_map<std::string,NetworKit::node>& gotermsID2idx, robin_hood::unordered_map<std::string,Root>& namespace2root, int& threads);
 
 class Annotation{
-    public:
+public:
     Annotation(){
 
     }
@@ -125,12 +126,12 @@ class Annotation{
     }
     void setAnnotation(NetworKit::node go, robin_hood::unordered_set<std::string>& genes){
 
-            std::vector<std::string> vec(genes.begin(),genes.end());
-            this->go2gvec.insert({go,vec});
+        std::vector<std::string> vec(genes.begin(),genes.end());
+        this->go2gvec.insert({go,vec});
 
         for(std::string g : genes){
             this->go2gvec.at(go).push_back(g);
-            if(this->getGOTerms(g).size()==0){
+            if(this->gene2govec.find(g)==this->gene2govec.end()){
                 robin_hood::unordered_set<NetworKit::node> vec;
                 this->gene2govec.insert({g,vec});
             }
@@ -139,22 +140,24 @@ class Annotation{
     }
     std::vector<std::string> getGenes(NetworKit::node& goTerm){
         if(this->go2gvec.find(goTerm) != this->go2gvec.end()){
-           return this->go2gvec.at(goTerm);
+            return this->go2gvec.at(goTerm);
         }else{
             return std::vector<std::string>();
         }
 
     }
-    robin_hood::unordered_set<NetworKit::node> getGOTerms(std::string& gene){
-      if(this->gene2govec.find(gene)!= this->gene2govec.end()){
+    robin_hood::unordered_set<NetworKit::node>& getGOTerms(std::string& gene){
         return this->gene2govec.at(gene);
-    }else{
-        return robin_hood::unordered_set<NetworKit::node>();
     }
+    int findGene(std::string& gene){
+        return this->gene2govec.find(gene)!=gene2govec.end();
     }
-    private:
-        robin_hood::unordered_map<NetworKit::node, std::vector<std::string>> go2gvec;
-        robin_hood::unordered_map<std::string, robin_hood::unordered_set<NetworKit::node>> gene2govec;
+    int findGO(NetworKit::node& go){
+        return this->go2gvec.find(go)!=go2gvec.end();
+    }
+private:
+    robin_hood::unordered_map<NetworKit::node, std::vector<std::string>> go2gvec;
+    robin_hood::unordered_map<std::string, robin_hood::unordered_set<NetworKit::node>> gene2govec;
 };
 
 
